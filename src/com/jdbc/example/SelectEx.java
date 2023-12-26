3d3e8e28e2c34df81e67329e3bee37dcc8576006
siho <xihoqazg@gmail.com>
2023-12-26 12:22:42 +0900

    first commit

---
+++ src/com/jdbc/example/SelectEx.java
@@ -0,0 +1,26 @@
+package com.jdbc.example;
+
+import java.sql.DriverManager;
+
+public class SelectEx {
+	public static void main(String[] args) {
+		
+		
+		
+		
+		
+		
+	}
+	
+	//select할 내용 작성 
+	public static void selectOne() {
+	
+		
+		
+		
+		
+		
+	}
+	
+	
+}
